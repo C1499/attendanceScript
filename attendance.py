784ee80a304ad1510715c88f9a122df4955eb91f
Cookies <1499832767@qq.com>
2020-05-11 10:51:46 +0800

    更新可选列数功能

--- attendance.py
+++ attendance.py
@@ -1,5 +1,5 @@
 import csv
-import xlrd3
+import xlrd3 as xlrd
     
 def today(file):
     l = []
@@ -10,20 +10,24 @@ def today(file):
     f.close()
     return l
 
-def peoples(file):
+def peoples(file,column):
     peopleList = []
-    wb = xlrd3.open_workbook(filename=file)
+    wb = xlrd.open_workbook(filename=file)
     sheet1 = wb.sheet_by_index(0)
     for i in range(1,sheet1.nrows):
-        peopleList.append(sheet1.row(i)[2].value)
+        # sheet1.row(i)[列数]
+        peopleList.append(sheet1.row(i)[int(column)].value)
     wb.release_resources()
     return peopleList
 
 def judge():
-    peopleList = peoples(input("请输入成员列表文件名（带后缀名，xlsx格式）\n"))
+    file = input("请输入成员列表文件名（带后缀名，xlsx格式）\n")
+    column = input("请输入成员名所在列数\n")
+    peopleList = peoples(file,column)
     #需要删除的成员名
-    peopleList.remove("")
+    # peopleList.remove("")
     todayList = today(input("请输入考勤列表文件名（带后缀名，csv格式）\n"))
+    t = input("请输入筛选时长\n")
     rpeople = peopleList.copy()
     repeat = []
     i=0
@@ -32,7 +36,8 @@ def judge():
         if rows:
             for people in peopleList:
                 if(people in rows[0] and len(rows)>1):
-                    if(int(rows[2])<170):
+                    # <数值
+                    if(int(rows[2])<int(t)):
                         print("时长异常：",people,rows[2],rows[3])
                     if(people in rpeople):
                         rpeople.remove(people)
